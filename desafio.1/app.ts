77580d46db85c9e047cc77d9c469be39d361c9e2
Cláudio Santos <107519290+Nivadinho@users.noreply.github.com>
2022-07-26 19:31:51 -0300

    Type-Javascript
    Desafio de Projeto a introdução ao type-javascript

---
+++ desafio.1/app.ts
@@ -0,0 +1,19 @@
+// desafio.1 = Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+
+
+// Resposta 1
+
+const funcionario = {
+    codigo: 10,
+    nome: 'João'
+};
+
+
+
+
+
+
